7b7387a7fc2991c6f697b965f7d031d845a272aa
a1767352 <a1767352@ac1c0196-ca05-47d2-82a5-85efdb603651>
2020-10-25 15:37:58 +0000

    added pointers to refer to other towns so that they dont exponentially grow

--- 2020/s2/pssd/week10practice/BlockEnemy.hpp
+++ 2020/s2/pssd/week10practice/BlockEnemy.hpp
@@ -1,7 +1,7 @@
 // BlockEnemy.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-//#include "pch.h"
+#include "pch.h"
 #include <iostream>
 #include <vector>
 #include <string>
@@ -10,13 +10,20 @@
 class Town
 {
 public:
-	Town() {};
-	
-	int townNum = 0;
-	std::vector<Town> connectedTowns;
+	std::vector<Town*> connectedTowns;
 	std::vector<int> townTravelCost;
 
-	~Town() {};
+	int townNum = 0;
+
+	Town() 
+	{ 
+		//std::cout << "created town\n"; 
+	};
+
+	~Town() 
+	{
+		//std::cout << "destroyed town\n";
+	};
 };
 
 class BlockEnemy
@@ -26,21 +33,33 @@ public:
 
 	BlockEnemy() {};
 
-	void parseTowns(std::vector<std::string> roads)
+	void parseTowns(std::vector<std::string> roads, int N)
 	{
+		towns.resize(N);
+
 		for (int i = 0; i < roads.size() + 1; i++)
 		{
 			Town temp;
 			towns.push_back(temp);
 		}
 
+		std::string a = "";
+		std::string b = "";
+		std::string c = "";
+
+		int next = 0;
+
+		int aInt = 0;
+		int bInt = 0;
+		int cInt = 0;
+
 		for (int i = 0; i < roads.size(); i++)
 		{
-			int next = 0;
+			next = 0;
 
-			std::string a = "";
-			std::string b = "";
-			std::string c = "";
+			a = "";
+			b = "";
+			c = "";
 
 			for (int j = 0; j < roads[i].size(); j++)
 			{
@@ -50,56 +69,52 @@ public:
 				}
 				else
 				{
-					switch (next)
+					if (next == 0)
 					{
-						case 0:
-						{
-							a.push_back(roads[i].at(j));
-							break;
-						}
-						case 1:
-						{
-							b.push_back(roads[i].at(j));
-							break;
-						}
-						case 2:
-						{
-							c.push_back(roads[i].at(j));
-							break;
-						}
-						default:
-						{
-							break;
-						}
+						a.push_back(roads[i].at(j));
+
+					}
+					if (next == 1)
+					{
+						b.push_back(roads[i].at(j));
+
+					}
+					if (next == 2)
+					{
+						c.push_back(roads[i].at(j));
 					}
 				}
 			}
 			
-			//std::cout << "town " << std::stoi(b) << " stored into town " << std::stoi(a) << "\n";
-			//std::cout << "town " << std::stoi(a) << " stored into town " << std::stoi(b) << "\n";
+			aInt = std::stoi(a);
+			bInt = std::stoi(b);
+			cInt = std::stoi(c);
 
-			towns[std::stoi(a)].townNum = std::stoi(a);
-			towns[std::stoi(b)].townNum = std::stoi(b);
+			//std::cout << "town " << bInt << " stored into town " << aInt << "\n";
+			//std::cout << "town " << aInt << " stored into town " << bInt << "\n";
 
-			towns[std::stoi(a)].townTravelCost.push_back(std::stoi(c));
-			towns[std::stoi(b)].townTravelCost.push_back(std::stoi(c));
+			towns[aInt].townNum = aInt;
+			towns[bInt].townNum = bInt;
 
-			towns[std::stoi(a)].connectedTowns.push_back(towns[std::stoi(b)]);
-			towns[std::stoi(b)].connectedTowns.push_back(towns[std::stoi(a)]);
+			towns[aInt].townTravelCost.push_back(cInt);
+			towns[bInt].townTravelCost.push_back(cInt);
+
+			towns[aInt].connectedTowns.push_back(&towns[bInt]);
+			towns[bInt].connectedTowns.push_back(&towns[aInt]);
 		}
 	}
 
 	int minEffort(int N, std::vector<std::string> roads, std::vector<int> occupiedTowns)
 	{
 		towns.clear();
-		parseTowns(roads);
+		parseTowns(roads, N);
 		
 		for (int i = 0; i < towns.size(); i++)
 		{
 			//std::cout << "Town " << i << " is connected to town(s): ";
 			for (int j = 0; j < towns[i].connectedTowns.size(); j++)
 			{
-				//std::cout << towns[i].connectedTowns[j].townNum << " (cost " << towns[i].townTravelCost[j] << ") ";
+				//std::cout << towns[i].connectedTowns[j]->townNum << " (cost " << towns[i].townTravelCost[j] << ") ";
 			}
 			//std::cout << "\n";
 		}
@@ -158,7 +173,7 @@ public:
 
 					for (int j = 0; j < towns[stack.top()].connectedTowns.size(); j++)
 					{
-						if (towns[stack.top()].connectedTowns[j].townNum == towns[lastTown].townNum)
+						if (towns[stack.top()].connectedTowns[j]->townNum == towns[lastTown].townNum)
 						{
 							lowest = stack.top();
 
@@ -180,7 +195,7 @@ public:
 					{
 						for (int j = 0; j < towns[stack.top()].connectedTowns.size(); j++)
 						{
-							if (towns[stack.top()].connectedTowns[j].townNum == towns[lastTown].townNum)
+							if (towns[stack.top()].connectedTowns[j]->townNum == towns[lastTown].townNum)
 							{
 								
 								//std::cout << "lastTown: " << towns[lastTown].townNum << "\n";
@@ -206,14 +221,14 @@ public:
 					//std::cout << "lowest cost was: " << cost << "\n";
 					totalCost += cost;
 
-					//std::cout << "removing town: " << towns[lowest].connectedTowns[disconnectPath].townNum << " from town: " << towns[lowest].townNum << "\n";
+					//std::cout << "removing town: " << towns[lowest].connectedTowns[disconnectPath]->townNum << " from town: " << towns[lowest].townNum << "\n";
 
 					towns[lowest].connectedTowns.erase(towns[lowest].connectedTowns.begin() + disconnectPath);
 					towns[lowest].townTravelCost.erase(towns[lowest].townTravelCost.begin() + disconnectPath);
 
 					for (int j = 0; j < towns[lowestPartner].connectedTowns.size(); j++)
 					{
-						if (towns[lowestPartner].connectedTowns[j].townNum == towns[lowest].townNum)
+						if (towns[lowestPartner].connectedTowns[j]->townNum == towns[lowest].townNum)
 						{
 							towns[lowestPartner].connectedTowns.erase(towns[lowestPartner].connectedTowns.begin() + j);
 							towns[lowestPartner].townTravelCost.erase(towns[lowestPartner].townTravelCost.begin() + j);
@@ -229,16 +244,16 @@ public:
 
 	bool search(int goal, int lastVisit, int currentVisit, std::stack<int> * stack)
 	{
-		//std::cout << "visiting: " << towns[lastVisit].connectedTowns[currentVisit].townNum << " with " << towns[towns[lastVisit].connectedTowns[currentVisit].townNum].connectedTowns.size() << " paths.\n";
-		stack->push(towns[towns[lastVisit].connectedTowns[currentVisit].townNum].townNum);
+		//std::cout << "visiting: " << towns[lastVisit].connectedTowns[currentVisit]->townNum << " with " << towns[towns[lastVisit].connectedTowns[currentVisit]->townNum].connectedTowns.size() << " paths.\n";
+		stack->push(towns[towns[lastVisit].connectedTowns[currentVisit]->townNum].townNum);
 
-		if (towns[towns[lastVisit].connectedTowns[currentVisit].townNum].townNum == goal)
+		if (towns[towns[lastVisit].connectedTowns[currentVisit]->townNum].townNum == goal)
 		{
 			//std::cout << "found the goal!\n";
 			return true;
 		}
 
-		if (towns[towns[lastVisit].connectedTowns[currentVisit].townNum].connectedTowns.size() == 1)
+		if (towns[towns[lastVisit].connectedTowns[currentVisit]->townNum].connectedTowns.size() == 1)
 		{
 			//std::cout << "found a dead end.\n";
 			stack->pop();
@@ -247,11 +262,11 @@ public:
 
 		bool result = false;
 
-		for (int j = 0; j < towns[towns[lastVisit].connectedTowns[currentVisit].townNum].connectedTowns.size(); j++)
+		for (int j = 0; j < towns[towns[lastVisit].connectedTowns[currentVisit]->townNum].connectedTowns.size(); j++)
 		{
-			if (towns[towns[lastVisit].connectedTowns[currentVisit].townNum].connectedTowns[j].townNum != towns[lastVisit].townNum)
+			if (towns[towns[lastVisit].connectedTowns[currentVisit]->townNum].connectedTowns[j]->townNum != towns[lastVisit].townNum)
 			{
-				result = search(goal, towns[towns[lastVisit].connectedTowns[currentVisit].townNum].townNum, j, stack);
+				result = search(goal, towns[towns[lastVisit].connectedTowns[currentVisit]->townNum].townNum, j, stack);
 
 				if (result == true)
 				{
@@ -276,13 +291,14 @@ int main()
 {
 	BlockEnemy test;
 	
-	std::cout << test.minEffort(5, { "1 0 1", "1 2 2", "0 3 3", "4 0 4" }, { 3, 2, 4 }) << "\n";
-	std::cout << test.minEffort(5, { "1 0 1", "1 2 2", "0 3 3", "4 0 4" }, { 3 }) << "\n";
-	std::cout << test.minEffort(12, { "0 1 3", "2 0 5", "1 3 1", "1 4 8", "1 5 4", "2 6 2", "4 7 11", "4 8 10", "6 9 7", "6 10 9", "6 11 6" }, { 1, 2, 6, 8 }) << "\n";
-	std::cout << test.minEffort(12, { "0 1 3", "2 0 5", "1 3 1", "1 4 8", "1 5 4", "2 6 2", "4 7 11", "4 8 10", "6 9 7", "6 10 9", "6 11 6" }, { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 }) << "\n";
-	std::cout << test.minEffort(4, { "1 3 95", "2 0 71", "2 3 78" }, { 0, 3, 2 }) << "\n";
-	std::cout << test.minEffort(12, { "9 5 681", "11 1 398", "6 1 559", "10 3 48", "5 0 978", "0 1 301", "0 2 727", "7 1 609", "3 9 894", "8 10 122", "4 3 127" }, { 11, 8, 1, 6, 4, 7, 9 }) << "\n";
-	
+	//std::cout << test.minEffort(5, { "1 0 1", "1 2 2", "0 3 3", "4 0 4" }, { 3, 2, 4 }) << "\n";
+	//std::cout << test.minEffort(5, { "1 0 1", "1 2 2", "0 3 3", "4 0 4" }, { 3 }) << "\n";
+	//std::cout << test.minEffort(12, { "0 1 3", "2 0 5", "1 3 1", "1 4 8", "1 5 4", "2 6 2", "4 7 11", "4 8 10", "6 9 7", "6 10 9", "6 11 6" }, { 1, 2, 6, 8 }) << "\n";
+	//std::cout << test.minEffort(12, { "0 1 3", "2 0 5", "1 3 1", "1 4 8", "1 5 4", "2 6 2", "4 7 11", "4 8 10", "6 9 7", "6 10 9", "6 11 6" }, { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 }) << "\n";
+	//std::cout << test.minEffort(4, { "1 3 95", "2 0 71", "2 3 78" }, { 0, 3, 2 }) << "\n";
+	//std::cout << test.minEffort(12, { "9 5 681", "11 1 398", "6 1 559", "10 3 48", "5 0 978", "0 1 301", "0 2 727", "7 1 609", "3 9 894", "8 10 122", "4 3 127" }, { 11, 8, 1, 6, 4, 7, 9 }) << "\n";
+	//std::cout << test.minEffort(50, { "24 14 30585", "14 2 17913", "2 1 7185", "14 32 3524", "2 3 23893", "14 42 32358", "1 46 15102", "42 29 20974", "29 11 24873", "2 23 17418", "2 31 3231", "29 12 14447", "11 9 1113", "2 0 8315", "46 34 19524", "32 33 25625", "14 17 30017", "42 18 20966", "9 47 11888", "33 35 23186", "33 13 31662", "2 20 7048", "0 7 13660", "29 21 31953", "13 44 13667", "0 38 26165", "17 27 21876", "9 28 20388", "11 49 8853", "14 10 12501", "7 45 27097", "14 39 5165", "33 16 622", "42 4 20706", "34 36 30941", "28 22 12384", "33 15 32654", "13 19 748", "19 25 18571", "4 40 26503", "10 37 11397", "9 8 19221", "24 43 17180", "0 5 25278", "1 6 20571", "28 48 9621", "2 26 11138", "16 41 2423", "46 30 777" }, { 21, 49, 34, 16, 40, 22, 6, 26, 39, 5, 33, 19, 44, 48, 9, 0, 3, 17, 45, 47 });
+	//std::cout << test.minEffort(50, {"11 49 705350", "45 49 708106", "48 49 557507", "16 49 206030" , "46 49 681085",  "42 49 592851", "29 49 955456", "36 49 644055", "14 49 998371", "25 49 243780", "38 49 676165", "19 49 295695", "37 49 85890", "34 49 772321", "31 49 495278", "35 49 881683", "32 49 509966", "13 49 573322", "41 49 953193", "28 49 684833", "8 49 338711", "22 49 7607", "20 49 988937", "15 49 774045", "12 49 749572", "23 49 197707", "18 49 143647", "30 49 913598", "24 49 784195", "27 49 589280", "17 49 864709", "6 49 689371", "9 49 287833", "21 49 93643", "3 49 628920", "4 49 280025", "7 49 877184", "26 49 161234", "5 49 273188", "0 49 543685", "1 49 960792", "40 49 174364", "33 49 157069", "39 49 268121", "10 49 177457", "43 49 576795", "44 49 498155", "2 49 285189", "47 49 155944" }, { 0, 10, 20, 30, 40, 47 }) << "\n";
 
 	return 0;
 }
